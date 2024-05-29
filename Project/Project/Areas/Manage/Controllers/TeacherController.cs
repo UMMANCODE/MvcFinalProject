de9014704537d8364e558d6bf75e103bfc59ab0d
UMMANCODE <ummanmm@code.edu.az>
2024-05-29 23:48:01 +0400

    Probably Last

--- Project/Project/Areas/Manage/Controllers/TeacherController.cs
+++ Project/Project/Areas/Manage/Controllers/TeacherController.cs
@@ -9,117 +9,153 @@ using Project.Models.Enums;
 using Project.ViewModels;
 
 namespace Project.Areas.Manage.Controllers {
-	[Area("manage")]
-	[Authorize(Roles = "superadmin, admin")]
-	public class TeacherController : Controller {
-		private readonly AppDbContext _context;
-		private readonly IWebHostEnvironment _env;
+  [Area("manage")]
+  [Authorize(Roles = "superadmin, admin")]
+  public class TeacherController : Controller {
+    private readonly AppDbContext _context;
+    private readonly IWebHostEnvironment _env;
 
-		public TeacherController(AppDbContext context, IWebHostEnvironment env) {
-			_context = context;
-			_env = env;
-		}
+    public TeacherController(AppDbContext context, IWebHostEnvironment env) {
+      _context = context;
+      _env = env;
+    }
 
-		public IActionResult Index(int page = 1) {
-			var query = _context.Teachers.AsQueryable();
+    public IActionResult Index(int page = 1) {
+      var query = _context.Teachers.AsQueryable();
 
-			var pageData = PaginatedList<Teacher>.Create(query, page, 3);
+      var pageData = PaginatedList<Teacher>.Create(query, page, 3);
 
-			if (pageData.TotalPages < page && pageData.TotalPages >= 1)
-				return RedirectToAction("index", new { page = pageData.TotalPages });
+      if (pageData.TotalPages < page && pageData.TotalPages >= 1)
+        return RedirectToAction("index", new { page = pageData.TotalPages });
 
-			return View(pageData);
-		}
+      return View(pageData);
+    }
 
-		public IActionResult Create() {
-			return View();
-		}
+    public IActionResult Create() {
+      return View();
+    }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Create(Teacher teacher) {
-			if (teacher.ImageFile == null) ModelState.AddModelError("ImageFile", "ImageFile is required!");
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Teacher teacher, List<Skill> Skills, List<Icon> Icons) {
+      if (teacher.ImageFile == null) {
+        ModelState.AddModelError("ImageFile", "ImageFile is required!");
+      }
 
-			if (!ModelState.IsValid) {
-				return View(teacher);
-			}
+      if (!ModelState.IsValid) {
+        return View(teacher);
+      }
 
-			teacher.ImageName = FileManager.Save(teacher.ImageFile, _env.WebRootPath, "img/teacher");
+      teacher.ImageName = FileManager.Save(teacher.ImageFile, _env.WebRootPath, "img/teacher");
 
-			_context.Teachers.Add(teacher);
-			_context.SaveChanges();
+      _context.Teachers.Add(teacher);
+      _context.SaveChanges();
 
-			return RedirectToAction("index");
-		}
+      foreach (var skill in Skills) {
+        var teacherSkill = new TeacherSkills {
+          TeacherId = teacher.Id,
+          Skill = skill
+        };
+        _context.TeacherSkills.Add(teacherSkill);
+      }
 
-		public IActionResult Edit(int id) {
-			var teacher = _context.Teachers
-				.Include(x => x.EventTeachers)
-				.Include(x => x.TeacherIcons)
-				.Include(x => x.TeacherSkills)
-				.FirstOrDefault(x => x.Id == id);
+      foreach (var icon in Icons) {
+        var teacherIcon = new TeacherIcons {
+          TeacherId = teacher.Id,
+          Icon = icon
+        };
+        _context.TeacherIcons.Add(teacherIcon);
+      }
 
-			if (teacher == null)
-				return RedirectToAction("notfound", "error");
+      _context.SaveChanges();
 
-			return View(teacher);
-		}
+      return RedirectToAction("Index");
+    }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Edit(Teacher teacher) {
-			if (!ModelState.IsValid) {
-				return View(teacher);
-			}
+    public IActionResult Edit(int id) {
+      var teacher = _context.Teachers
+        .Include(x => x.EventTeachers)
+        .Include(x => x.TeacherIcons)
+        .Include(x => x.TeacherSkills)
+        .FirstOrDefault(x => x.Id == id);
 
-			var existTeacher = _context.Teachers
-				.Include(x => x.EventTeachers)
-				.Include(x => x.TeacherIcons)
-				.Include(x => x.TeacherSkills)
-				.FirstOrDefault(x => x.Id == teacher.Id);
+      if (teacher == null)
+        return RedirectToAction("notfound", "error");
 
-			if (existTeacher == null) return RedirectToAction("notfound", "error");
+      return View(teacher);
+    }
 
-			string? deletedFile = null;
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Teacher teacher, List<TeacherSkills> TeacherSkills, List<TeacherIcons> TeacherIcons) {
+      if (!ModelState.IsValid) {
+        return View(teacher);
+      }
 
-			existTeacher.FullName = teacher.FullName;
-			existTeacher.Position = teacher.Position;
-			existTeacher.About = teacher.About;
-			existTeacher.Degree = teacher.Degree;
-			existTeacher.Experience = teacher.Experience;
-			existTeacher.Hobbies = teacher.Hobbies;
-			existTeacher.Faculty = teacher.Faculty;
-			existTeacher.Mail = teacher.Mail;
-			existTeacher.Phone = teacher.Phone;
-			existTeacher.Skype = teacher.Skype;
+      var existTeacher = _context.Teachers
+          .Include(x => x.EventTeachers)
+          .Include(x => x.TeacherIcons).ThenInclude(ti => ti.Icon)
+          .Include(x => x.TeacherSkills).ThenInclude(ts => ts.Skill)
+          .FirstOrDefault(x => x.Id == teacher.Id);
 
-			if (teacher.ImageFile != null) {
-				deletedFile = existTeacher.ImageName;
-				existTeacher.ImageName = FileManager.Save(teacher.ImageFile, _env.WebRootPath, "img/teacher");
-			}
+      if (existTeacher == null) return RedirectToAction("notfound", "error");
 
-			_context.Teachers.Update(existTeacher);
+      string? deletedFile = null;
 
-			if (deletedFile != null) {
-				FileManager.Delete(_env.WebRootPath, "img/teacher", deletedFile);
-			}
+      existTeacher.FullName = teacher.FullName;
+      existTeacher.Position = teacher.Position;
+      existTeacher.About = teacher.About;
+      existTeacher.Degree = teacher.Degree;
+      existTeacher.Experience = teacher.Experience;
+      existTeacher.Hobbies = teacher.Hobbies;
+      existTeacher.Faculty = teacher.Faculty;
+      existTeacher.Mail = teacher.Mail;
+      existTeacher.Phone = teacher.Phone;
+      existTeacher.Skype = teacher.Skype;
 
-			_context.SaveChanges();
+      if (teacher.ImageFile != null) {
+        deletedFile = existTeacher.ImageName;
+        existTeacher.ImageName = FileManager.Save(teacher.ImageFile, _env.WebRootPath, "img/teacher");
+      }
 
-			return RedirectToAction("index");
-		}
+      _context.Teachers.Update(existTeacher);
 
-		public IActionResult Delete(int id) {
-			var existTeacher = _context.Teachers.Find(id);
-			if (existTeacher == null) return NotFound();
+      var existingSkills = _context.TeacherSkills.Where(ts => ts.TeacherId == teacher.Id).ToList();
+      _context.TeacherSkills.RemoveRange(existingSkills);
 
-			_context.Teachers.Remove(existTeacher);
-			_context.SaveChanges();
+      foreach (var skill in TeacherSkills) {
+        skill.TeacherId = teacher.Id;
+        _context.TeacherSkills.Add(skill);
+      }
 
-			if (existTeacher.ImageName != null)
-				FileManager.Delete(_env.WebRootPath, "img/teacher", existTeacher.ImageName);
+      var existingIcons = _context.TeacherIcons.Where(ti => ti.TeacherId == teacher.Id).ToList();
+      _context.TeacherIcons.RemoveRange(existingIcons);
 
-			return RedirectToAction("index");
-		}
-	}
+      foreach (var icon in TeacherIcons) {
+        icon.TeacherId = teacher.Id;
+        _context.TeacherIcons.Add(icon);
+      }
+
+      if (deletedFile != null) {
+        FileManager.Delete(_env.WebRootPath, "img/teacher", deletedFile);
+      }
+
+      _context.SaveChanges();
+
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Delete(int id) {
+      var existTeacher = _context.Teachers.Find(id);
+      if (existTeacher == null) return NotFound();
+
+      _context.Teachers.Remove(existTeacher);
+      _context.SaveChanges();
+
+      if (existTeacher.ImageName != null)
+        FileManager.Delete(_env.WebRootPath, "img/teacher", existTeacher.ImageName);
+
+      return RedirectToAction("index");
+    }
+  }
 }
--- Project/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6ed8923fb25feedee65109ebbc69b8d2abfb63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3350ff54f5addc7320744bbf6f96e29b44188b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
